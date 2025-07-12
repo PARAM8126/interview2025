11afe94a215be9702956919d64715124107e39f3
PARAM8126 <Paramsharmaup@gmail.com>
2025-07-12 14:43:45 +0530

    12 july

---
+++ src/main/java/02_OOPs/interface/InterfaceDemo1.java
@@ -0,0 +1,39 @@
+interface Animal {
+    void bark();
+}
+
+interface Dog {
+    void eat();
+}
+
+class Puppy implements Animal, Dog {
+    public void bark() {
+        System.out.println("barking");
+    }
+
+    public void eat() {
+        System.out.println("eating");
+    }
+}
+
+public class InterfaceDemo1
+  {
+    public static void main(String[] args) {
+
+       
+        Animal obj = new Puppy();
+        obj.bark();
+
+        Dog d = new Puppy();
+        d.eat();
+
+       // OR use Puppy reference to call both methods
+        Puppy p = new Puppy();
+        p.bark();
+        p.eat();
+    }
+}
+
+      
+        
+        
